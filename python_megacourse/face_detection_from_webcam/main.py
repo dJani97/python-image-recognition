3dcd16609b44944bd0a69098b81b426a557dbfef
János Dobszai <dobszaijani@gmail.com>
2018-10-17 23:30:05 +0200

    implementing face detection

--- python_megacourse/face_detection_from_webcam/main.py
+++ python_megacourse/face_detection_from_webcam/main.py
@@ -2,9 +2,23 @@ import cv2 as cv
 
 face_cascade = cv.CascadeClassifier("../haarcascade_frontalface_default.xml")
 video = cv.VideoCapture(0)
+capturing = True
 
-"""
-the project will be built here
-"""
+while capturing:
+    check, frame = video.read()
+    gray_img = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray_img,
+                                          scaleFactor=1.2,
+                                          minNeighbors=5)
+
+    for x, y, w, h in faces:
+        img = cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    cv.imshow("Webcam 1", frame)
+    key = cv.waitKey(10)
+    if key == ord('q'):
+        capturing = False
 
 video.release()
+cv.destroyAllWindows()
