1ee7090574d7830179bbece5cfa7613eeaca4260
János Dobszai <dobszaijani@gmail.com>
2018-10-17 23:00:32 +0200

    face detection from image

---
+++ python_megacourse/face_detection_from_image/main.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+
+face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
+img = cv.imread("news.jpg")
+gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray_img,
+                                      scaleFactor=1.1,
+                                      minNeighbors=5)
+
+for x, y, w, h in faces:
+    img = cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+cv.imshow("Faces", img)
+cv.waitKey(0)
