97f1789084e73e947ff0d52798bb3aed576f2286
János Dobszai <dobszaijani@gmail.com>
2018-10-20 17:12:50 +0200

    script for removing blank images

--- sentdex_tutorials/creating_own_cascade/download-image-by-link.py
+++ sentdex_tutorials/creating_own_cascade/download-image-by-link.py
@@ -1,6 +1,6 @@
 import urllib.request
 import cv2
-import numpy
+import numpy as np
 import os
 
 
@@ -25,7 +25,23 @@ def store_raw_images(negative_images_link):
             print(str(e))
 
 
+def find_uglies():
+    match = False
+    for file_type in ['negative']:
+        for img in os.listdir(file_type):
+            for ugly in os.listdir('uglies'):
+                try:
+                    current_image_path = str(file_type) + '/' + str(img)
+                    ugly = cv2.imread('uglies/' + str(ugly))
+                    question = cv2.imread(current_image_path)
+                    if ugly.shape == question.shape and not (np.bitwise_xor(ugly, question).any()):
+                        print('That is one ugly pic! Deleting!')
+                        print(current_image_path)
+                        os.remove(current_image_path)
+                except Exception as e:
+                    print(str(e))
+
+
+find_uglies()
 
 # store_raw_images('http://image-net.org/api/text/imagenet.synset.geturls?wnid=n00523513')
-
-store_raw_images('http://image-net.org/api/text/imagenet.synset.geturls?wnid=n12992868')
